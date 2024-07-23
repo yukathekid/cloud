37b70515cdbd931fae3982710181ad7561d940f1
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-07-23 20:02:26 -0300

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -15,23 +15,16 @@ export default {
         const realUrl = `https://firebasestorage.googleapis.com/v0/b/hwfilm23.appspot.com/o/Anikodi%2F${folder}%2F${ep}.mp4?alt=media&token=${token}`;
 
         try {
-          // Faz a requisição para o URL do Firebase com o token
           const response = await fetch(realUrl, {
             method: request.method,
             headers: request.headers,
           });
 
-          if (response.ok) {
-            // Se a resposta for bem-sucedida, repasse o conteúdo
-            return new Response(response.body, {
-              status: response.status,
-              statusText: response.statusText,
-              headers: response.headers
-            });
-          } else {
-            // Se a resposta do Firebase indicar erro (como 403 ou 404), trate como erro
-            return new Response('Erro ao acessar o conteúdo.', { status: response.status });
-          }
+          return new Response(response.body, {
+            status: response.status,
+            statusText: response.statusText,
+            headers: response.headers
+          });
         } catch (error) {
           return new Response('Erro ao acessar o conteúdo.', { status: 500 });
         }
