4ceffcab63608db8e2561a955170f0b301b00094
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-07-24 12:32:43 -0300

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,44 +1,29 @@
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
-    console.log('Request URL:', request.url);
 
     // Novo bloco para camuflagem de URL
-    const hlsPathPattern = /^\/cdn\/hls\/([^\/]+)\/([^\/]+)$/;
+    const hlsPathPattern = /^\/cdn\/hls\/([^\/]+)\/([^\/]+)\/master\.txt$/;
     const match = url.pathname.match(hlsPathPattern);
 
     if (match) {
       const anime = match[1];
       const episode = match[2];
 
-      // URL do JSON
-      const jsonUrl = `https://firebasestorage.googleapis.com/v0/b/hwfilm23.appspot.com/o/Anikodi%2F${anime}%2Fmaster.json?alt=media`;
-      console.log('Fetching JSON URL:', jsonUrl);
+      const realUrl = `https://firebasestorage.googleapis.com/v0/b/hwfilm23.appspot.com/o/Anikodi%2F${anime}%2F${episode}.mp4?alt=media`;
 
       try {
-        // Buscar o JSON
-        const jsonResponse = await fetch(jsonUrl);
-        if (!jsonResponse.ok) {
-          console.error(`JSON fetch failed with status: ${jsonResponse.status}`);
-          return new Response('Erro ao acessar o JSON.', { status: 500 });
-        }
-        const jsonData = await jsonResponse.json();
-        console.log('JSON Data:', jsonData);
+        const response = await fetch(realUrl, {
+          method: request.method,
+          headers: request.headers,
+        });
 
-        // Obter informações do episódio
-        const episodeData = jsonData[episode];
-        if (episodeData) {
-          const token = episodeData.token;
-          const camouflagedUrl = `https://cloud.anikodi.xyz/cdn/hls/${anime}/${token}`;
-          console.log('Camouflaged URL:', camouflagedUrl);
-
-          // Redirecionar para a URL camuflada
-          return Response.redirect(camouflagedUrl, 302);
-        } else {
-          return new Response('Episódio não encontrado.', { status: 404 });
-        }
+        return new Response(response.body, {
+          status: response.status,
+          statusText: response.statusText,
+          headers: response.headers
+        });
       } catch (error) {
-        console.error('Error:', error.message);
         return new Response('Erro ao acessar o conteúdo.', { status: 500 });
       }
     }
