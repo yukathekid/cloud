f8f06f8afaaec7da3355730eb3ec8f4acd7fe796
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-07-24 13:30:28 -0300

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,19 +2,30 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
 
-    // Novo bloco para camuflagem de URL com base64
-    const hlsPathPattern = /^\/cdn\/hls\/([a-zA-Z0-9+/=]+)$/;
+    // Novo bloco para camuflagem de URL com MD5
+    const hlsPathPattern = /^\/cdn\/hls\/([^\/]+)\/([a-fA-F0-9]{32})$/;
     const match = url.pathname.match(hlsPathPattern);
 
     if (match) {
-      const base64Path = match[1];
-      const decodedPath = atob(base64Path); // Decodifica o base64
-      const [anime, episode] = decodedPath.split('/');
+      const anime = match[1];
+      const md5Hash = match[2];
 
-      if (anime && episode) {
-        const realUrl = `https://firebasestorage.googleapis.com/v0/b/hwfilm23.appspot.com/o/Anikodi%2F${anime}%2F${episode}.mp4?alt=media`;
+      // URL para o arquivo JSON do Firebase
+      const jsonUrl = `https://firebasestorage.googleapis.com/v0/b/hwfilm23.appspot.com/o/Anikodi%2F${anime}%2Fmaster.json?alt=media`;
 
-        try {
+      try {
+        const jsonResponse = await fetch(jsonUrl);
+        if (!jsonResponse.ok) {
+          return new Response('Erro ao acessar o mapeamento de dados.', { status: 500 });
+        }
+
+        const jsonData = await jsonResponse.json();
+        const decodedPath = Object.values(jsonData).flat().find(entry => entry[md5Hash]);
+
+        if (decodedPath) {
+          const [animePath, episode] = decodedPath.split('/');
+          const realUrl = `https://firebasestorage.googleapis.com/v0/b/hwfilm23.appspot.com/o/Anikodi%2F${animePath}%2F${episode}.mp4?alt=media`;
+
           const response = await fetch(realUrl, {
             method: request.method,
             headers: request.headers,
@@ -25,11 +36,11 @@ export default {
             statusText: response.statusText,
             headers: response.headers
           });
-        } catch (error) {
-          return new Response('Erro ao acessar o conteúdo.', { status: 500 });
+        } else {
+          return new Response('Caminho inválido.', { status: 400 });
         }
-      } else {
-        return new Response('Caminho inválido.', { status: 400 });
+      } catch (error) {
+        return new Response('Erro ao acessar o conteúdo.', { status: 500 });
       }
     }
 
