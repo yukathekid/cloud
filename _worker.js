6790f2b4d9e88f2afd0fcc6cab504bb73f027dd0
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-07-28 20:32:43 -0300

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -13,25 +13,20 @@ export default {
       // Construir a URL real do M3U8
       const realUrl = `https://firebasestorage.googleapis.com/v0/b/hwfilm23.appspot.com/o/Anikodi%2F${animeName}%2F${ep}.m3u8?alt=media`;
 
-      try {
-        // Buscar o conteúdo do M3U8 original
-        const response = await fetch(realUrl);
-        if (!response.ok) {
-          return new Response('Erro ao buscar o M3U8', { status: 500 });
-        }
+      // Buscar o conteúdo do M3U8
+      const response = await fetch(realUrl);
 
-        const m3u8Content = await response.text();
-
-        // Retornar o conteúdo do M3U8
-        return new Response(m3u8Content, {
-          status: 200,
-          headers: {
-            'Content-Type': 'application/vnd.apple.mpegurl'
-          }
-        });
-      } catch (error) {
-        return new Response('Erro interno', { status: 500 });
+      if (!response.ok) {
+        return new Response('Não foi possível obter o conteúdo do vídeo.', { status: 404 });
       }
+
+      // Retornar o conteúdo do M3U8
+      const contentType = response.headers.get('Content-Type') || 'application/vnd.apple.mpegurl';
+      return new Response(await response.text(), {
+        headers: {
+          'Content-Type': contentType,
+        },
+      });
     }
 
     // Deixa outras requisições serem tratadas pelo Cloudflare Pages e _redirects
