02ba37cbd888177ad7181df3aa490dd4ddfa48e6
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-07-28 17:14:09 -0300

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -14,7 +14,21 @@ export default {
       const realUrl = `https://firebasestorage.googleapis.com/v0/b/hwfilm23.appspot.com/o/Anikodi%2F${animeName}%2F${ep}.m3u8?alt=media`;
 
       // Fazer o redirecionamento
-      return fetch(realUrl);
+      try {
+        const videoResponse = await fetch(realUrl);
+        
+        if (!videoResponse.ok) {
+          return new Response('Video fetch error.', { status: 500 });
+        }
+
+        // Retornar a resposta diretamente, preservando os headers e o corpo
+        return new Response(videoResponse.body, {
+          status: videoResponse.status,
+          headers: videoResponse.headers
+        });
+      } catch (error) {
+        return new Response(`Error fetching video: ${error.message}`, { status: 500 });
+      }
     }
 
     // Deixa outras requisições serem tratadas pelo Cloudflare Pages e _redirects
