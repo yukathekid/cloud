590f8b7040aed5dcd0d99058546fc66bdfef1d44
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-07-29 12:27:55 -0300

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,24 +5,25 @@ export default {
     const pathname = url.pathname;
 
     // Verifica se o caminho corresponde ao padrão esperado
-    const match = pathname.match(/^\/cdn\/([^\/]+)\/(\d+)$/);
+    const match = pathname.match(/^\/cdn\/([^\/]+)\/(\d+)\/([^\/]+)$/);
     if (match) {
         const animeName = match[1];
-        const ep = match[2];
+        const folder = match[2];
+        const tsFile = match[3];
 
         // Constrói a URL real do Firebase Storage
-        const firebaseUrl = `https://firebasestorage.googleapis.com/v0/b/hwfilm23.appspot.com/o/Anikodi%2F${encodeURIComponent(animeName)}%2F${ep}.m3u8?alt=media`;
+        const firebaseUrl = `https://firebasestorage.googleapis.com/v0/b/hwfilm23.appspot.com/o/Anikodi%2F${encodeURIComponent(animeName)}%2F${folder}%2F${tsFile}?alt=media`;
 
         // Faz a solicitação para a URL real
         const response = await fetch(firebaseUrl);
 
         if (!response.ok) {
-            return new Response('Erro ao buscar o vídeo', { status: response.status });
+            return new Response('Erro ao buscar o arquivo', { status: response.status });
         }
 
         // Copia os headers da resposta original e adiciona headers adicionais
         const newHeaders = new Headers(response.headers);
-        newHeaders.set('Content-Type', 'application/vnd.apple.mpegurl');
+        newHeaders.set('Content-Type', 'video/mp2t');
         newHeaders.set('Access-Control-Allow-Origin', '*');
 
         // Retorna a resposta do Firebase Storage com os headers ajustados
